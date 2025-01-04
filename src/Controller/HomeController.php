7e1a783ed83d20730c4bd2c2acb8acb72dffe373
Adrian Florczak <kontakt@adrianflorczak.pl>
2025-01-04 15:14:44 +0100

    First Controllers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,7 @@ namespace Aflorczak\CommunityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
